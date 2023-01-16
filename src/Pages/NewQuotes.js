8a75138e4121a5d840c2598fb62f67a50c1e0636
Mansoor Khan <mansoorKh1098@gmail.com>
2023-01-16 17:01:01 +0530

    ✨ feat: sending data

--- src/Pages/NewQuotes.js
+++ src/Pages/NewQuotes.js
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import QuoteForm from "../components/quotes/QuoteForm";
+import useHttp from "../hooks/use-http";
+import { addQuote } from "../lib/api";
 
 const NewQuotes = () => {
+  const { sendRequest, status } = useHttp(addQuote);
   const history = useHistory();
-  const addingNeqQuote = (newData) => {
-    console.log(newData);
 
-    history.push("/quotes");
+  useEffect(() => {
+    if (status == "completed") {
+      history.push("/quotes");
+    }
+  }, [status, history]);
+  const addingNeqQuote = (newData) => {
+    sendRequest(newData);
   };
   return (
     <div>
-      <QuoteForm onAddQuote={addingNeqQuote} />
+      <QuoteForm isLoading={status == "pending"} onAddQuote={addingNeqQuote} />
     </div>
   );
 };
